5df42e69d9f89547173e61f971dfe38e6339d57a
Bishwaranjan Sandhu <bsa@byggtjeneste.no>
2025-05-30 15:39:04 +0200

    using azure open ai env

--- 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
+++ 02-SetupDevEnvironment/src/BasicChat-01MEAI/Program.cs
@@ -1,11 +1,29 @@
-﻿using Azure;
-using Azure.AI.Inference;
+﻿using Azure.AI.OpenAI;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using System.ClientModel;
 
-IChatClient client = new ChatCompletionsClient(
-        endpoint: new Uri("https://models.inference.ai.azure.com"),
-        new AzureKeyCredential(Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? throw new InvalidOperationException("Missing GITHUB_TOKEN environment variable. Ensure you followed the instructions to setup a GitHub Token to use GitHub Models.")))
-        .AsIChatClient("Phi-3.5-MoE-instruct");
+var deploymentName = "gpt-4o-mini"; // e.g. "gpt-4o-mini"
+var endpoint = new Uri("https://Bt-Az-OpenAi.openai.azure.com/"); // e.g. "https://< your hub name >.openai.azure.com/"
+
+// Build configuration to access user secrets
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var apiKeyValue = Environment.GetEnvironmentVariable("AZURE_AI_KEY");
+if (string.IsNullOrEmpty(apiKeyValue))
+{
+    apiKeyValue = config["AZURE_AI_KEY"];
+}
+
+var apiKey = new ApiKeyCredential(apiKeyValue);
+
+IChatClient client = new AzureOpenAIClient(
+    endpoint,
+    apiKey).GetChatClient(deploymentName).AsIChatClient();
+
+
 
 var response = await client.GetResponseAsync("What is AI?");
 
